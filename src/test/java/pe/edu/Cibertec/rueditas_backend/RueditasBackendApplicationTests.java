9cd91328eec55f4309bbb44ea7ce35798842e658
Sahir Ricse <ricse.sahir@gmail.com>
2024-09-24 12:14:48 -0500

    Creación del proyecto backend

---
+++ src/test/java/pe/edu/Cibertec/rueditas_backend/RueditasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.Cibertec.rueditas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
